42999efd9b5d33eea6ee03529fd8b6400c7c8c1f
Risatoar <risatoar@163.com>
2017-11-30 22:59:48 +0800

    首页消息组件完成

---
+++ auction/AuctionWeb/src/components/news.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div class="message">
+			<div class="message-inner">
+				<div class="message-inner-head">
+					<a>信</a>
+					<a>息</a>
+					<a>中</a>
+					<a>心</a>
+					<div class="line"></div>
+					<p>随时掌握行业动态 即刻了解热点资讯</p>
+				</div>
+				<div class="message-inner-body">
+					<div class="message-inner-body-left">
+						<hotnews></hotnews>
+					</div>
+					<div class="message-inner-body-right">
+						<auctioninfo></auctioninfo>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import hotnews from './news/hotnews.vue'
+import auctioninfo from './news/auctionInfo.vue'
+export default {
+	components: {
+		hotnews,
+		auctioninfo
+	}
+}
+</script>
+
+<style scoped>
+	@import '/static/css/news.css'
+</style>
---
+++ auction/AuctionWeb/src/components/news/auctionInfo.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<div class="auctioninfo">
+			<div class="auctioninfo-head">
+				<a class="head-words">拍卖公告</a>
+				<a class="head-more">更多</a>
+			</div>
+			<div class="auctioninfo-body" v-for="(item,index) in auctioninfo">
+				<div class="auctioninfo-body-item">
+					<span class="new">NEW</span>
+					<li class="item-title"><a href="http://www.alltobid.com/contents/13/2158.html">{{ item.title }}</a></li>
+					<li class="item time">拍卖时间：{{ item.time }}</li>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				auctioninfo: [
+					{
+						time: '2017/11/30',
+						title: '2017年12月8日物资网络在线拍卖'
+					},
+					{
+						time: '2017/11/30',
+						title: '2017年12月8日物资网络在线拍卖'
+					},
+					{
+						time: '2017/11/30',
+						title: '2017年12月8日物资网络在线拍卖'
+					},
+					{
+						time: '2017/11/30',
+						title: '2017年12月8日物资网络在线拍卖'
+					},
+					{
+						time: '2017/11/30',
+						title: '2017年12月8日物资网络在线拍卖'
+					},
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+@import '/static/css/auctioninfo.css'
+</style>
